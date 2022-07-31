94d349229e628ff3f4c483adf2691dc4361971f9
mdafrozz <106298595+mdafrozz@users.noreply.github.com>
2022-07-31 19:30:47 +0530

    Add: Program to demonstrate File Operations

---
+++ FileOperations.java
@@ -0,0 +1,133 @@
+/**
+ * 
+ */
+package com.bridgelabz.main;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @author MD_AFROZ
+ *
+ */
+public class FileOperations {
+	static Scanner sc;
+	static String path = "C:/Users/MD_AFROZ/eclipse-workspace/EmployeePayrollService/FileOperations";
+
+	public void createNewFilesWithUserInput() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter the desired name of your file: "); 
+		String fileName = sc.nextLine();
+		File file = new File(path);
+		File actualFile = new File(file, fileName);
+		try {
+			if (actualFile.createNewFile()) {
+				System.out.println("File created.");
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void showAllFiles() {
+
+		File directoryPath = new File(path);
+		for (File file : directoryPath.listFiles()) {
+			System.out.println(file.getName());
+		}
+
+		System.out.println("\n<------------Show Text Files------------>");
+		File[] txtFiles = directoryPath.listFiles(new FilenameFilter() {
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".txt");
+			}
+		});
+
+		for (File file : txtFiles) {
+			System.out.println(file.getName());
+		}
+
+	}
+
+	public void deleteFiles() {
+		System.out.println("<-------------Delete Files and Directories------------->");
+		File directoryPath = new File(path);
+		for (File file : directoryPath.listFiles()) {
+			System.out.println(file.getName());
+		}
+
+		sc = new Scanner(System.in);
+		System.out.print("Enter the Filename to delete: ");
+		String fileName = sc.nextLine();
+
+		File file = new File(path);
+		File actualFile = new File(file, fileName);
+
+		if (actualFile.delete()) {
+			System.out.println("File is deleted successfully...");
+		} else {
+			System.out.println("File is not found.!!!");
+		}
+	}
+
+	public void createDirectories() {
+		sc = new Scanner(System.in);
+		System.out.print("Enter the name to create a directory: ");
+		String directoryName = sc.nextLine();
+
+		File file = new File(path);
+		File actualFile = new File(file, directoryName);
+
+		if (actualFile.mkdir()) {
+			System.out.println("Directory is created successfully...");
+		} else {
+			System.out.println("Directory name is already existed.!!!");
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("***************** JAVA IO *****************");
+		boolean exit = false;
+		Scanner sc = new Scanner(System.in);
+		FileOperations services = new FileOperations();
+
+		while (!exit) {
+			System.out.println("<----------------------------------------------------------->");
+			System.out.println(" 1.Create New File\n 2.Show All Files\n 3.Delete Files\n 4.Create New Directory\n 5.Quit");
+			System.out.println("<--------------------Choose your options-------------------->");
+			int option = sc.nextInt();
+
+			switch (option) {
+			case 1:
+				System.out.println("<-------------Create New File------------->");
+				services.createNewFilesWithUserInput();
+				break;
+			case 2:
+				System.out.println("<-------------Show All Files------------->");
+				services.showAllFiles();
+				break;
+			case 3:
+				System.out.println("<-------------Delete Files------------->");
+				services.deleteFiles();
+				break;
+			case 4:
+				System.out.println("<-------------Create New Directory------------->");
+				services.createDirectories();
+				break;
+			case 5:
+				exit = true;
+				System.out.println("********Thank You********");
+				break;
+			default:
+				System.out.println("Invalid option.Please try again!!!");
+			}
+		}
+
+		sc.close();
+
+	}
+}
